5a6b9fd613f8468a46d6fa6b9ec7b82d9e893105
luiarthur <luiarthur@yahoo.com>
2013-09-02 12:17:40 -0600

    working on mh

--- protein/Assign_2.2/mh.scala
+++ protein/Assign_2.2/mh.scala
@@ -16,19 +16,26 @@ object mh {
       pw.close()
     }
   }
+  class q(t:Double){
+    def prob{
+    }
+    def draw{
+    }
+  }
 
   def main(args:Array[String]){
     
     //  INITIALIZE:
     val N = 15000; val koIn = 8; val dIn = 4; var cnt = 0
-    var M = Array.fill(15000)("")
+    var M = new Array[String](15000)
     val myFile = "~/Documents/LuiDahl/protein/Assign2.2/mhResults.txt"
     M.update(0,Rho.draw(ko=koIn,d=dIn))
     //  END INITIALIZE
 
     for (i <- 1 to (N-1)){
       M.update(i, M(i-1) )
-      val cand = q.draw(M(i))
+      val t = new Random().nextDouble
+      val cand = q.draw(M(i),t)
       val r = Rho.prob(cand,ko=koIn,d=dIn) / Rho.prob(M(i),ko=koIn,d=dIn) * 
               q.prob(M(i),cand,ko=koIn,d=dIn) / q.prob(cand,M(i),ko=koIn,d=dIn) 
       if (r > new Random().nextDouble){
