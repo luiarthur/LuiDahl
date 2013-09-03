2c1c4b7d2ea08fe8a9113fd84b63b9d74fe5c496
luiarthur <luiarthur@yahoo.com>
2013-09-02 19:32:55 -0600

    Worked on mh for a day

--- protein/Assign_2.2/mh.scala
+++ protein/Assign_2.2/mh.scala
@@ -16,6 +16,9 @@ import Rho._
     }
   }
 
+  // Questions:
+  //   How do I write the probabilty for merge and split?
+  //   How do I write merge function?
   class q {
     def prob(a:String,b:String,ko:Int,d:Int,t:Int):Double={
       if (t <=.5){1}
@@ -24,7 +27,7 @@ import Rho._
     }
     def draw(a:String,t:Double):String={
       val m = inEta(a).sum
-      val g = new Random().nextInt(m-2) + 1
+      var g = new Random().nextInt(m-2) + 1
       var blks =Array('H','E','T','C')
 
       if (t <= .25) {
@@ -39,14 +42,14 @@ import Rho._
         getRho(getEL(a) updated (g, newBlk))
       }
 
-      else if (t <= .5) {
+      else if (t <= .5) { // switch & change bnd. pos. function
         val z = getEL(a)(g-1)._2+getEL(a)(g)._2-1
         val p = new Random().nextInt(z)+1
         getRho ((getEL(a) updated (g-1, (getEL(a)(g)._1,p) ) ) updated
                 (m, (getEL(a)(g)._1,z-p) ) )
       }
 
-      else if (t <= .75) {
+      else if (t <= .75) { // split function
         val z = getEL(a)(g)._2 - 1
         val p = new Random().nextInt(z) + 1
         if ( (a(g-1)==blks(i)) || (a(g)==blks(i)) ){
@@ -56,10 +59,17 @@ import Rho._
         val r = new Random().nextInt(blks.size)
         var tempEL = getEL(a) updated (g, (getEL(a)(g)._1,z)) 
         tempEL = (tempEL.dropRight(m-g) :+ (blks(r),z-p) ) ++ tempEL.drop(g)        
+        getRho(tempEL)
       }
 
-      else {
-        
+      else { // merge function is very hard.
+        var tempEL = getEL (a)
+        // m-3 & + 2 because I don't want to merge into the 0th element (C)
+	while (tempEL(g-1)._1==tempEL(g+1)._1){g = new Random().nextInt(m-3) + 2} 
+        tempEL = tempEL updated (g+1, (tempEL(g+1)._1,
+                                 tempEL(g)._2+tempEL(g+1)._2))
+        tempEL = tempEL.splitAt(g)._1 ++ tempEL.splitAt(g).drop(1)
+        getRho(tempEL)
       }
 
     }
@@ -69,7 +79,7 @@ import Rho._
     
     //  INITIALIZE:
     val N = 15000; val koIn = 8; val dIn = 4; var cnt = 0
-    var M = new Array[String](15000)
+    var M = new Array[String](N)
     val myFile = "~/Documents/LuiDahl/protein/Assign2.2/mhResults.txt"
     M.update(0,Rho.draw(ko=koIn,d=dIn))
     //  END INITIALIZE
