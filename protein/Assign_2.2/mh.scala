7c7f812aa92fa288d63c26f389d61b4e1b64d1cf
luiarthur <luiarthur@yahoo.com>
2013-09-11 23:20:36 -0600

    mh is syntax free. But need to find bugs

--- protein/Assign_2.2/mh.scala
+++ protein/Assign_2.2/mh.scala
@@ -30,12 +30,13 @@ object mh {
   // NEED TO ADD Rho. before each function of Rho
   class Proposal {
 
-    def draw(a:String,t:Double):String={
-      val m = inEta(a).sum
+    def draw(a:String,t:Double):(String, Double, Double)={
+      val m = Rho.inEta(a).sum
       var g = ran.nextInt(m-2) + 1
       var blks =Array('H','E','T','C')
 
-      def switch():String= { // switch function
+      def switch():(String, Double, Double)=
+      { // switch function
         for (i <- 0 to (blks.length-1)){
           if ( (a(g-1)==blks(i)) || (a(g)==blks(i)) || (a(g+1)==blks(i)) ){
             blks.update(i,'Z')
@@ -43,46 +44,60 @@ object mh {
         }
         blks = blks.filter (s => s!='Z')
         val r = ran.nextInt(blks.size) 
-        val newBlk = (blks(r),getEL(a)(g)._2)
-        (getRho(getEL(a) updated (g, newBlk)), 1, 1)
+        val newBlk = (blks(r),Rho.getEL(a)(g)._2)
+        (Rho.getRho(Rho.getEL(a) updated (g, newBlk)), 1.0, 1.0)
+      }
+      
+
+      // NEEDS FIXING. OutOfBound ERROR:
+      def changeBound():(String, Double, Double) =
+      { // change boundary position function
+        val z = Rho.getEL(a)(g-1)._2 + Rho.getEL(a)(g)._2
+        val p = ran.nextInt(z-1) + 1
+        //println("DID A CHANGEBOUND "+m+" "+g+" "+p+" "+z+" "+Rho.getEL(a))
+        (Rho.getRho((Rho.getEL(a) updated (g-1, (Rho.getEL(a)(g-1)._1,p) ) ) updated
+                (g, (Rho.getEL(a)(g)._1,z-p) )), 1.0, 1.0 )
       }
 
-      def changeBound():String= { // change boundary position function
-        val z = getEL(a)(g-1)._2+getEL(a)(g)._2-1
-        val p = ran.nextInt(z)+1
-        (getRho ((getEL(a) updated (g-1, (getEL(a)(g)._1,p) ) ) updated
-                (m, (getEL(a)(g)._1,z-p) )), 1, 1 )
-      }
 
-      def split(from:String, to:String):String= { // split function
-        val z = getEL(a)(g)._2 - 1
+      def split(from:String, to:String):(String, Double, Double) = 
+      { // split function
+        val z = Rho.getEL(a)(g)._2 - 1
         val p = ran.nextInt(z) + 1
         for (i <- 0 to (blks.length-1)){
           if ( (a(g-1)==blks(i)) || (a(g)==blks(i)) ){ blks.update(i,'Z') }
         }
         blks = blks.filter (s => s!='Z')
         val r = ran.nextInt(blks.size)
-        var tempEL = getEL(a) updated (g, (getEL(a)(g)._1,z)) 
-        tempEL = (tempEL.dropRight(m-g) :+ (blks(r),z-p) ) ++ tempEL.drop(g)        
-        val b = getRho(tempEL)
-        (b,.25/(m-2)/(inEta(a)._g-1)/(blks.length), merge(from=b, to=a)._2)
+        var tempEL = Rho.getEL(a) updated (g, (Rho.getEL(a)(g)._1,p)) 
+        tempEL = (tempEL.dropRight(m-g) :+ (blks(r),z-p)) ++ tempEL.drop(g)        
+        val b = Rho.getRho(tempEL)
+        //(b,.25/(m-2)/(Rho.inEta(a)._g-1)/(blks.length), merge(from=b, to=a)._2)
+        val c = if (to == "") {merge(from=b, to=a)._2} else {1}
+        (b, .25/(m-2)/z/(blks.length), c)
       }
 
-      def merge(from:String, to:String):String={ // merge function is very hard.
-        var tempEL = getEL (a)
-        // m-3 & + 2 because I don't want to merge into the 0th element (C)
-	      while (tempEL(g-1)._1==tempEL(g+1)._1){g = ran.nextInt(m-3) + 2} 
-        tempEL = tempEL updated (g+1, (tempEL(g+1)._1,
-                                 tempEL(g)._2+tempEL(g+1)._2))
-        tempEL = tempEL.splitAt(g)._1 ++ tempEL.splitAt(g).drop(1)
-        val b = getRho(tempEL)
-        (b, .5*.25/(m-3), split(b,a)._2)
+      def merge(from:String, to:String):(String, Double, Double)=
+      { // merge function is very hard.
+        var tempEL = Rho.getEL(a)
+        if (m <= 3) { (from,1,1) }
+        else
+        {
+          // m-3 & + 2 because I don't want to merge into the 0th element (C)
+          while (tempEL(g-1)._1==tempEL(g+1)._1){g = ran.nextInt(m-3) + 2} 
+          tempEL = tempEL updated (g+1, (tempEL(g+1)._1,
+                                   tempEL(g)._2+tempEL(g+1)._2))
+          tempEL = tempEL.splitAt(g)._1 ++ tempEL.splitAt(g+1)._2
+          val b = Rho.getRho(tempEL)
+          val c = if (to == "") {split(b,a)._2} else {1}
+          (b, .5*.25/(m-3), c)
+        }
       }
       
       if (t <= .25) { switch()}
       else if (t <= .5) { changeBound() }
-      else if (t <= .75) { split( from =_, to =_) }
-      else { merge( from =_, to =_) }
+      else if (t <= .75) { split( from =a, to ="" ) }
+      else { merge( from =a, to ="" ) }
 
     }
   }
@@ -90,25 +105,31 @@ object mh {
   def main(args:Array[String]){
     
     //  INITIALIZE:
-    val N = 15000; val koIn = 8; val dIn = 4; var cnt = 0
+    val N = 150000; val koIn = 8; val dIn = 4; var cnt = 0
     var M = new Array[String](N)
-    val myFile = "~/Documents/LuiDahl/protein/Assign2.2/mhResults.txt"
-    M.update(0,Rho.draw(ko=koIn,d=dIn))
+    val myFile = "mhResults.txt"
+    M.update(0,Rho.draw(N=1,ko=koIn,d=dIn)(0))
+    val proposal = new Proposal
+    for (i <- 1 to N/2000) {print("#")}; println()
     //  END INITIALIZE
 
-    for (i <- 1 to (N-1)){
+    for (i <- 1 to (N-1))
+    {
+      if ( (i-1)%2000 == 0 ) {print(">")}
       M.update(i, M(i-1) )
       val t = ran.nextDouble
-      val cpp = Proposal.draw(M(i),t)
-      cand = cpp._1; cand2Curr = cpp._2; curr2Cand = cpp._3
+      val cpp = proposal.draw(M(i),t)
+      val cand = cpp._1; val cand2Curr = cpp._2; val curr2Cand = cpp._3
       val r = Rho.prob(cand,koIn,dIn) / Rho.prob(M(i),koIn,dIn) * 
               cand2Curr / curr2Cand
-      if (r > ran.nextDouble){
+      if ((r > ran.nextDouble) & (cand.length==koIn)){
         M(i) = cand
         cnt += 1
       }
+      //println(M(i)+" "+cpp._1+" "+r)
     } 
-    println(cnt)
+    println()
+    println(cnt*1.0/N)
     outData(M.drop(5000),myFile)
   }
 }
--- protein/Assign_2.2/oldRho.scala
+++ protein/Assign_2.2/oldRho.scala
@@ -8,17 +8,15 @@
 //   sequences like CEEETTTC and CTTTEEEC appear less frequently than they
 //   ought to. That leads me to think there is a problem with my
 //   draw function.
-// Creating too many objects. Especially when randomly draw from distributions.
-// Condsider making  only one object to draw from.
 
 package Rho
 
 import scala.collection.immutable.Vector.empty
 import org.apache.commons.math3.special.Gamma._
-import breeze.stats.distributions.{NegativeBinomial,Binomial,Gaussian}
+import breeze.stats.distributions._
 import scala.io.Source
 import java.io.File
-import scala.math.{log,pow,Pi,exp}
+import scala.math._
 import util.Random
 
 class Rho{
@@ -28,9 +26,12 @@ class Rho{
   // Don't create objects every time! Takes much more time.
 
   val theta = Vector(.170207, .224021, .178348, .427424)
+  def rnbinom(mean: Double, size: Double):Int={
+    val x = new NegativeBinomial(size,mean/(size+mean)).draw
+    x
+  }
   
-    
-  val ran = new Random() //JUST ADDED
+  //rMult is working fine
   def rMult(n: Int, v: Vector[Double]):Vector[Int]={
     var N = n; var x = Vector.fill(0)(0)
     val p = Vector.tabulate(v.size)(i => v(i)/(1-v.dropRight(v.size-i).sum))
@@ -42,9 +43,6 @@ class Rho{
     x
   }
   def rLamGivenEta (eta: Char):Int= {
-    def rnbinom(mean: Double, size: Double):Int={
-      new NegativeBinomial(size,mean/(size+mean)).draw
-    }
     if (eta=='H'){5+rnbinom(1.885880,6.953392)}
     else if (eta=='E'){3+rnbinom(2.521091,2.899121)}
     else if (eta=='T'){3+rnbinom(0.839557,0.728294)}
@@ -62,7 +60,7 @@ class Rho{
   def mSamp (ko:Int):Int={
     val k = ko/.62; val d = 4
     //val k = ko; val d =4
-    //val r = new Random().nextDouble*
+    val r = new Random().nextDouble
     def f(x:Double):Double={
       new Gaussian(2.347253+.154154*k, 5.526382/d).pdf(x) 
     }
@@ -75,58 +73,46 @@ class Rho{
     }
     vf = vf:+1.0
     var i = 0
-    while (ran.nextDouble > vf(i)) i += 1
+    while (r > vf(i)) i += 1
     i+3
-  }  
-  //////////////////////////////////////////////////////////
-  def draw(N: Int, ko:Int=8, d:Int=4):Array[String]={
-    var arRho = new Array[String] (N)
-    for ( i <- 1 to (N/200) ) print("#"); println()
+  }
+   
+  def draw(ko:Int=8, d:Int=4):String={
+    var rho = ""
+    var valid = false
+    var etas = ""
+    while (!valid){
+      valid = true
+      rho = ""; etas = ""
+      //val m = (new Random().nextGaussian*(5.526382/d) + 2.347253+.154154*k).toInt
+      val m = mSamp(ko)
+      val mv = rMult(m-2,theta)
+      val tempEL = Vector( ('H',mv(0)), ('E',mv(1)), ('T',mv(2)), ('C',mv(3)) )
+      val tempRho = getRho(tempEL)
+      etas = "C".concat(Random.shuffle(tempRho.toSeq).mkString :+ 'C')
 
-    for ( n <- 0 to (N-1) ){
-	    var rho = ""
-	    var valid = false
-	    var etas = ""
-	    while (!valid){
-	      valid = true
-	      rho = ""; etas = ""
-	      //val m = (new Random().nextGaussian*(5.526382/d) + 2.347253+.154154*k).toInt
-	      val m = mSamp(ko)
-	      val mv = rMult(m-2,theta)
-	      val tempEL = Vector( ('H',mv(0)), ('E',mv(1)), ('T',mv(2)), ('C',mv(3)) )
-	      val tempRho = getRho(tempEL)
-	      etas = "C".concat(Random.shuffle(tempRho.toSeq).mkString :+ 'C')
-
-       // NOT SURE IF THIS CHUNK IS USEFUL
-        for (i <- 1 to (etas.length-2) )
-        {
-          if (etas(i)==etas(i-1))
-          {
-            for (j <- i to (etas.length-2) )
-            {
-              if ( (etas.charAt(j)!=etas(i-1)) & (etas(j)!=etas(i+1)) &
-                   (etas.charAt(i)!=etas(j-1)) & (etas(i)!=etas(j+1)) )
-              {
-                val t = etas.charAt(j) 
-                etas = etas updated (j, etas(i)) 
-                etas = etas updated (i, t)
-              } 
+     // NOT SURE IF THIS CHUNK IS USEFUL
+      for (i <- 1 to (etas.length-2) ){
+        if (etas(i)==etas(i-1)){
+          for (j <- i to (etas.length-2) ){
+            if ( (etas.charAt(j)!=etas(i-1)) & (etas(j)!=etas(i+1)) &
+                 (etas.charAt(i)!=etas(j-1)) & (etas(i)!=etas(j+1)) ){
+              val t = etas.charAt(j); 
+              etas = etas updated (j, etas(i)) 
+              etas = etas updated (i, t)
             } 
           } 
-        }// END OF THIS CHUNK
+        } 
+      }// END OF THIS CHUNK
 
-        for (i <- 1 to (etas.length-1)){
-          if (etas(i-1)==etas(i)){valid=false}
-        }
-        val EL = Vector.tabulate(etas.length)( i => (etas(i),rLamGivenEta(etas(i))) )
-        rho = getRho(EL)
-        if (rho.length != ko){valid=false}
+      for (i <- 1 to (etas.length-1)){
+        if (etas(i-1)==etas(i)){valid=false}
       }
-      arRho.update(n,rho)
-      if ( n % 200 == 0 ) print(">")
+      val EL = Vector.tabulate(etas.length)( i => (etas(i),rLamGivenEta(etas(i))) )
+      rho = getRho(EL)
+      if (rho.length != ko){valid=false}
     }
-    println()
-    arRho
+    rho
   }
 //pRho////////////////////////////////////////////////////////////////
   // checked countEta
@@ -141,9 +127,10 @@ class Rho{
   } 
 
   // checked inEta
+  // checked inEta
   def inEta (rv:String):Vector[Int]={
     val mv = Vector(countEta(rv,'H'),countEta(rv,'E'),
-                    countEta(rv,'T'),countEta(rv,'C')) 
+                   countEta(rv,'T'),countEta(rv,'C')) 
     mv
   }
 
@@ -168,18 +155,14 @@ class Rho{
   def sumLogLamGivenEta (EL:Vector[(Char,Int)]):Double={
     def logLamGivenEta (el: (Char,Int)):Double={
       if ((el._1=='H')&(el._2>=5)){logNegBinom(el._2-5,mean=1.885880,size=6.953392)}
-      else if ((el._1=='E')&(el._2>=3)){logNegBinom(el._2-3,mean=2.521091,size=2.899121)}
-      else if ((el._1=='T')&(el._2>=3)){logNegBinom(el._2-3,mean=0.839557,size=0.728294)}
-      else if ((el._1=='C')&(el._2>=1)){logNegBinom(el._2-1,mean=0.990796,size=3.725501)}
+      else if ((el._1=='E')&(el._2>=3)){logNegBinom(el._2-3,mean=2.521091,size=2.8991)}
+      else if ((el._1=='T')&(el._2>=3)){logNegBinom(el._2-3,mean=0.839557,size=0.7282)}
+      else if ((el._1=='C')&(el._2>=1)){logNegBinom(el._2-1,mean=0.990796,size=3.7255)}
       else {0}
     }
-    var sumLog = 0.0; var bad = false
-    //EL.foreach( el => (sumLog = sumLog+logLamGivenEta(el)))
-    for ( i <- 0 to (EL.size-1) ){
-      if (logLamGivenEta(EL(i))==0) bad = true
-      sumLog = sumLog + logLamGivenEta(EL(i)) 
-    }
-    if (bad) 0 else sumLog
+    var sumLog = 0.0
+    EL.foreach( el => (sumLog = sumLog+logLamGivenEta(el)))
+    sumLog
   } 
   def logNorm(x:Double,mu:Double,sig:Double):Double={
     -.5*(log(2*Pi*sig*sig)+pow((x-mu)/sig,2))
@@ -197,8 +180,8 @@ class Rho{
     val m = inEta(rho).sum
     val mv = inEta(rho).dropRight(1):+(inEta(rho)(3)-2)
     val EL = getEL(rho)
-    val ans = exp(logNorm(m,2.347253+.154154*k, 5.526382/d)+logMult(mv,theta)+sumLogLamGivenEta(EL)) 
-    if (sumLogLamGivenEta(EL) == 0) 0 else ans
+    val ans = exp(logNorm(m,2.347253+.154154*k, 5.526382/d)+logMult(mv,theta)+sumLogLamGivenEta(EL))
+    ans
   }
 }
 object Rho extends Rho
@@ -207,3 +190,4 @@ object Rho extends Rho
 //To run rRho:      scala -cp classes Rho
 //TO Compile ALL:   scalac -d classes -cp classes *.scala
 //To run test:      scala -cp classes test
+
